7c99750ced2011809c4337ed3b6e5c4271796730
Mareike <mareikedatascience@gmail.com>
2020-12-30 09:53:15 +0100

    Prepare for Heroku - Comment out app.run()

--- worldbank_fertility.py
+++ worldbank_fertility.py
@@ -1,5 +1,2 @@
 from worldbank_fertility_app import app
-app.run(host='0.0.0.0', port=3001, debug=True)
-# for local execution:
-# host=127.0.0.1
-# port=5000
+#app.run(host='127.0.0.1', port=5000, debug=True)
