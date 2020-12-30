5a203eef73cce2dc3b12b4e70e0037811dbd2521
Mareike <mareikedatascience@gmail.com>
2020-12-30 09:22:50 +0100

    Update host & port

--- worldbank_fertility.py
+++ worldbank_fertility.py
@@ -1,4 +1,5 @@
 from worldbank_fertility_app import app
-app.run(host='127.0.0.1', port=5000, debug=True)
-#0.0.0.0
-#3001
+app.run(host='0.0.0.0', port=3001, debug=True)
+# for local execution:
+# host=127.0.0.1
+# port=5000
